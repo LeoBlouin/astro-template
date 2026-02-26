b7af7ad04efca36d097e854e4cc2622ffd0b346e
Léo Blouin <leoblouinpro@gmail.com>
2026-02-26 15:09:07 +0100

    setup(project): Tailwind + Layout + Header + Footer + Constants

---
+++ src/data/navigation.ts
@@ -0,0 +1,6 @@
+export const NAVIGATION_LINKS = [
+  { name: 'Accueil', href: '/' },
+  { name: 'Services', href: '#services' },
+  { name: 'À propos', href: '#about' },
+  { name: 'Contact', href: '#contact' },
+];
---
+++ src/data/site-config.ts
@@ -0,0 +1,22 @@
+export const SITE_CONFIG = {
+  siteName: 'Mon Agence Web',
+  description: "Expert en [Métier] à [Ville]. Plus de 10 ans d'expérience.",
+  lang: 'fr',
+  author: 'Léo Blouin',
+  url: 'https://www.monsite-client.fr', // Important pour les balises OG et Sitemap
+  image: '/default-og-image.png', // Image par défaut dans public/
+
+  // Coordonnées pour le futur Footer et la page Contact
+  contact: {
+    email: 'contact@client.fr',
+    phone: '01 23 45 67 89',
+    address: '123 rue de la Paix, 75000 Paris',
+    googleMapsLink: 'https://goo.gl/maps/...',
+  },
+
+  // Réseaux sociaux
+  social: {
+    facebook: 'https://facebook.com/client',
+    instagram: 'https://instagram.com/client',
+  },
+};
