13c25f46a70e37fa1464272483efb59455355902
Léo Blouin <leoblouinpro@gmail.com>
2026-02-27 08:24:50 +0100

    fix(ui): Fix UI elements

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,5 +1,5 @@
 export const NAVIGATION_LINKS = [
-  { name: 'Accueil', href: '/' },
+  { name: 'Accueil', href: '#hero' },
   { name: 'Services', href: '#services' },
   { name: 'À propos', href: '#about' },
   { name: 'Contact', href: '#contact' },
